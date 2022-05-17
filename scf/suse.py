d966c90b53219cef32727d1bdc3bdb4d66bf39c4
dadav <33197631+dadav@users.noreply.github.com>
2022-05-17 21:22:23 +0200

    feat: use browser ua

--- scf/suse.py
+++ scf/suse.py
@@ -45,7 +45,12 @@ def url_to_soup(url: str, timeout: int = 30, use_cache: bool = True):
     if not use_cache:
         cache.cache.delete_url(url)
 
-    res = cache.get(url, timeout=timeout, verify=settings.connection.ssl_verify)
+    user_agent = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:100.0) Gecko/20100101 Firefox/100.0'}
+
+    res = cache.get(url,
+                    headers=user_agent,
+                    timeout=timeout,
+                    verify=settings.connection.ssl_verify)
 
     res.raise_for_status()
 
