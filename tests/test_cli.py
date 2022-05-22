92ce015fbe9abc57564515247533800420844131
dadav <33197631+dadav@users.noreply.github.com>
2022-05-22 18:01:34 +0200

    test: fix ws

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -15,11 +15,3 @@ def test_version():
     result = runner.invoke(cli.app, ["--version"])
     assert result.exit_code == 0
     assert f"v{__version__}\n" in result.stdout
-
-
-# def test_server():
-#     """
-#     Test if the correct version is printed
-#     """
-#     result = runner.invoke(cli.app, ["server"])
-#     assert result.exit_code == 0
